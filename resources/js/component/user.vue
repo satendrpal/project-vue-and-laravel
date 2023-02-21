563628817fd8b459a942d33e2136b8749cf7b589
Your satendrapal <satendrap270@gmail.com>
2023-02-21 14:52:09 +0530

    first commit

---
+++ resources/js/component/user.vue
@@ -0,0 +1,65 @@
+<template>
+    <Header />
+    <div class="Sidebar">
+  <div id="fetch">
+    <table class="table table-striped " id="table">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+         <th>Email</th>
+        </tr>
+      </thead>
+      <tr v-for="product in products" :key="product">
+             
+             <td>{{ product.id}}</td>
+             <td>{{ product.name }}</td>
+             <td>{{ product.email }}</td>
+             <td>
+               <!-- <img v-bind:src="`../uploads/products/${product.file}`" class="rounded-circle bg-dark" width="180" height=""> -->
+             </td>
+             <td>
+               <button @click="deleteProduct(product.id)" class="btn btn-danger mx-3" ><i class="fa fa-trash"></i></button>
+              
+               <!-- <button class="btn btn-danger" @click="deleteProduct(product.id)"> 
+                 Delete
+               </button> -->
+               <!-- <button class="btn btn-success" @click="Edit(product.id)" ><i class="fa fa-edit"></i></button> -->
+               <div class="btn-group" role="group">
+                 <router-link
+                   :to="{ name: 'Edit', params: { id: product.id } }"
+                   class="btn btn-success"
+                   ><i class="fa fa-edit" target="__black"></i></router-link
+                 >
+               </div>
+               </td>
+               </tr>
+      </table>
+      </div>
+      </div>
+
+</template>
+<script>
+import Header from './Header.vue'
+export default {
+    name:"user",
+    components:{
+         Header,
+    },
+       data(){
+
+       },
+       created(){
+         
+         axios.get("/api/userdata").then((response) => {
+                 this.products = response.data.data;
+                 console.log(this.products);
+               })
+               .catch((error) => {
+                 console.log(error);
+                 this.products = [];
+               });
+           },
+
+}
+</script>
